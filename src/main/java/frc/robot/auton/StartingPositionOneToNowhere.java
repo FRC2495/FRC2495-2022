10df7b50667fe81443f4b29a533700595e6fbb76
frc2495 <frc2495@gmail.com>
2022-01-26 19:50:46 -0500

    added nowhere anuton

---
+++ src/main/java/frc/robot/auton/StartingPositionOneToNowhere.java
@@ -0,0 +1,20 @@
+package frc.robot.auton;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.commands.*;
+import frc.robot.auton.AutonConstants;
+
+
+public class StartingPositionOneToNowhere extends CommandGroup {
+	/**
+	 * Add your docs here.
+	 */
+    final int TURN_DIRECTION = +1;
+        //Left is equal to -1
+        //Right is equal to +1
+
+	public StartingPositionOneToNowhere() {
+
+  }
+
+}
