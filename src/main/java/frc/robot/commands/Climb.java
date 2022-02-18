e80de0b03d4a603536194aec772206276a3d3dfb
frc2495 <frc2495@gmail.com>
2022-02-18 18:38:40 -0500

    Implemented climb commands

--- src/main/java/frc/robot/commands/Climb.java
+++ src/main/java/frc/robot/commands/Climb.java
@@ -16,9 +16,51 @@ public class Climb extends CommandGroup {
 	 */
 	public Climb() {
 
-        addSequential(new FrontArmsExtend());
+        addSequential(new RearArmsExtend());
+
+        // move forward
+
+        addSequential(new RearArmsRetract()); 
+
+        // move onto the bar
     
         addSequential(new FrontElbowsOpen());
+
+        addSequential(new FrontArmsExtend());
+
+        addSequential(new FrontElbowsOpen());
+
+        addSequential(new FrontArmsExtend());
+
+        addSequential(new FrontElbowsClose());
+
+        addSequential(new RearArmsExtend());
+
+        addSequential(new RearElbowsClose());
+
+        addSequential(new RearArmsRetract());
+
+        addSequential(new RearArmsExtend());
+
+        addSequential(new FrontArmsExtend());
+
+        addSequential(new FrontElbowsClose());
+
+        addSequential(new FrontArmsRetract());
+
+        addSequential(new FrontElbowsOpen());
+
+        addSequential(new RearArmsExtend());
+
+        addSequential(new FrontArmsExtend());
+
+        addSequential(new RearElbowsOpen());
+
+        addSequential(new FrontArmsRetract());
+
+        addSequential(new FrontElbowsClose());
+
+        addSequential(new RearArmsExtend());
 
         // todo: finish implementation
    
